8dae93b67349a18e14e8b47f1ad8c33bd265042f
124c4a11 <124c4a11@mail.ru>
2019-01-21 11:44:51 +0300

    Footer: base markup

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,18 @@
   <div id="app">
     <pp-header />
     <router-view/>
+    <pp-footer />
   </div>
 </template>
 
 <script>
 import Header from '@/components/Header'
+import Footer from '@/components/Footer'
 
 export default {
   components: {
-    ppHeader: Header
+    ppHeader: Header,
+    ppFooter: Footer
   }
 }
 </script>
---
+++ src/components/Footer.vue
@@ -0,0 +1,17 @@
+<template>
+  <footer class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container">
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item active">
+          <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Menu</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Admin</a>
+        </li>
+      </ul>
+    </div>
+  </footer>
+</template>
